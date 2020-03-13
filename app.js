f3d0a52e17bce3990a9115ed36341c2d525f6980
nani$;)))) <NANI@MacBook-Pro-3.local>
2020-03-12 23:51:10 -0400

    idk

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
-
+var compression = require('compression');
 var app = express();
-
+app.use(compression());
 var mongoose = require('mongoose');
 var dev_db_url = 'mongodb+srv://ncarcana:frankie19@cluster0-rbwmq.mongodb.net/local_library?retryWrites=true&w=majority';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
@@ -25,6 +26,7 @@ if (port == null || port == "") {
   port = 8000;
 }
 app.listen(port);
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
